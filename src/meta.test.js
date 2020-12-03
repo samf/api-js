2fcd1a278c51e48020880a2453af8fcd257f86ed
Sam Falkner <sam.falkner@gmail.com>
2020-12-03 07:02:21 -0700

    test the meta via mockAxios

--- src/meta.test.js
+++ src/meta.test.js
@@ -6,7 +6,22 @@ afterEach(() => {
 });
 
 it("sets ename and apibase", async () => {
-  const meta = new Meta();
+  const metap = new Meta();
 
   expect(mockAxios.head).toHaveBeenCalled();
+
+  const ename = "firehouse";
+  const apibase = "/efmrl-api/";
+  mockAxios.mockResponse({
+    headers: {
+      "x-efmrl-name": ename,
+      "x-efmrl-api": "/efmrl-api/",
+    },
+  });
+
+  const meta = await metap;
+
+  expect(meta.subefmrl).toBe(false);
+  expect(meta.ename).toBe(ename);
+  expect(meta.apibase).toBe(apibase);
 });
