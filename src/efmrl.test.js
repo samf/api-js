289b908ff1297563274eb3966471a11456fec5a0
Sam Falkner <sam.falkner@gmail.com>
2020-12-08 20:26:43 -0700

    efmrl.test.js: mock the meta class

--- src/efmrl.test.js
+++ src/efmrl.test.js
@@ -1,8 +1,16 @@
 import mockAxios from "jest-mock-axios";
 import { Efmrl } from "./efmrl.js";
+import { Meta } from "./meta.js";
+
+jest.mock("./meta");
+
+beforeEach(() => {
+  Meta.mockClear();
+});
 
 it("gets the meta", async () => {
   const eh = new Efmrl();
 
   expect(eh.meta).toBeTruthy();
+  expect(Meta).toHaveBeenCalledTimes(1);
 });
